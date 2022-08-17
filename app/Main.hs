1a2ece6b6e2747885032fc7122e0fb7954f54ba0
galen <galen.sprout@gmail.com>
2022-08-17 05:49:21 +0000

    thinking about how to recurse through links so that I can audit validity

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,140 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Main where
 
 import Scrappy.Requests
 import Scrappy.Scrape
 import Scrappy.Elem.ChainHTML
 import Scrappy.Elem.SimpleElemParser
+import Scrappy.Elem.Types (getHrefEl, innerHtmlFull, Elem')
+import Scrappy.Links
+import Scrappy.BuildActions (catEithers) -- TODO: Move to Scrappy.Types
+
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Text.Parsec (anyChar, (<|>), parse, many, try)
+import Control.Applicative (liftA2)
+import Data.Maybe (catMaybes)
+import Data.Text (Text)
 import Data.List
+
+import Data.Map 
+
+type Paragraph = Text
+-- data RetraceableTree = RetraceableTree (Map Link ([Paragraph], RetraceableTree))
+--   deriving (Semigroup, Monoid, Show)
+
+
+type RetraceableTree = Map Link ([Paragraph], RetraceableTree)
+
+type RetraceableTree' = Tree (Link [ElemHead])
+
+-- uncurry(1)
+-- >> Map Link ([Paragraph], Map Link 
+
+type Depth = Int 
+
+runTree :: (Depth, ElemHead, Link) -> IO RetraceableTree
+runTree depth eh link = do
+
+  -- Depth wouldn't actually affect the returned Tree just if it should stop
+  -- ie case depth + 1 > threshold -> pure [] 
+  
+  html <- getHtml mgr link
+  let res = scrape pTagWithLinks html
+
+
+  let label :: (Link, [ElemHead])
+
+  Node label forest
+
+  - the problem with this is that what elemhead gave each branch of the forest
+  - maybe I can use pTagWithLinks or generally, the link <a> tag to label each ( Link >>==> Forest )  
+
+  forest <- mapM runTree links
+  pure $ label forest 
+  -- a link should have a Label such that Label <- where it was found : PTag + ThisLink
+
+
+
+i could use FRP to model this
+
+ - An output is some paragraph
+ - two paragraphs dont necessarily relate here
+ - but two paragraphs may be acted on by two other forces of change
+   - in real terms, one set of  [paragraphs] ~ one page may be referenced by two different trees 
+
+
 
 main :: IO ()
 main = do
-  html <- getHtml' "https://dexscreener.com/"
-  let tickerPrices =
-        flip scrape html $ 
-        (el "a" [("class", "chakra-link css-1oo4dn7")])
-        `contains`
-        (el "span" [("class", "chakra-text css-njkgh9")] </>>= (el "span" [("class", "css-1k4xub7")]))
+  mgr <- newManager tlsManagerSettings
+  (_, html) <- getHtml mgr $ Link "https://en.wikipedia.org/wiki/Network_topology"
 
-  print $ flip scrape html $ (el "a" [("class", "chakra-link css-1oo4dn7")])
-  print $ isInfixOf "chakra-link css-1oo4dn7" html
-  print $ flip scrape html $ el "div" [("class", "css-y67qmp")]
-  pure ()
+
+  
+
+  let
+    res :: Maybe [(Elem' String, [Link])]
+    res = flip scrape html $ pTagWithLinks $ Link "https://en.wikipedia.org/wiki/Network_topology"
+
+  
+  runLeaf
+
+  [Link] >>= \link -> mapM_ recursiveFunc n links
+
+  - n : levels deep ; either approaches infinity or hits [] 
+  - but at this point I am a leaf 
+
+  - since i will quickly run into a memory overload as is, I can just have paragraphs be distinct IDs meaning the
+    tree will be:
+
+           type RecTree = Map Link ([ParaID], Map ParaID RecTree) -- Not all paragraphs will have links
+
+
+  - then: this is all gonna be run at the top level as a forest ~ list
+
+  successesM thisRecursiveFunction [@Genre]
+
+  --TODO(galen): should i make successesParM
+    
+  -- end data structure
+  
+
+  -- TODO
+      -- all text
+      -- all links
+      -- recursion into other pages 
+  
+  mapM_ print res 
   
   -- writeFile "results.txt" $ show $ maybe [] id tickerPrices
+
+
+-- Only since we care about the links here, otherwise this would be the parser that ignores style tags 
+pTagWithLinks :: Link -> ScraperT (Elem' String, [Link])
+pTagWithLinks cLink = do
+  e <- el "p" []
+  let txt = innerHtmlFull e
+  eiths <- case parse f "in p tag" txt of
+    Right x -> pure x 
+    --Right (words, links) -> pure (e { innerHtmlFull = words }, links)
+    Left _ -> error "idk ... weird" 
+
+
+  let words = g eiths
+  -- True because we'd like to stay on the same site 
+  let links = fmap (getHrefEl True cLink) (catEithers eiths)
+
+  pure (e { innerHtmlFull = words } , catMaybes links)             
+      
+
+  where
+    g (x:xs) = case x of
+      Right e -> innerHtmlFull e <> (g xs) 
+      Left chr -> chr : (g xs)
+    g [] = []
+    
+    f = many ((Right <$> (try $ el "a" [])) <|> (Left <$> anyChar))
+
+  
--- shell.nix
+++ shell.nix
@@ -1,35 +1,18 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
-
   inherit (nixpkgs) pkgs;
-  scrappySrc = pkgs.fetchFromGitHub {
-    owner = "Ace-Interview-Prep";
-    repo = "scrappy";
-    rev =  "dfa79ce72c921054d4906a833dbfc1e879c24b41";
-    sha256 = "1bl0n8bz461x5dal912768md2y8d8a0h5glgs5fj9hfb4p8kwkfw";
-  };
-  scrappy = pkgs.haskellPackages.callCabal2nix "scrappy" scrappySrc {}; 
-  f = { mkDerivation, base, lib }:
-      mkDerivation {
-        pname = "crypto-slu";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [ base scrappy ];
-        license = "unknown";
-        hydraPlatforms = lib.platforms.none;
-      };
-
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
-
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
+  wikiScraper = import ./default.nix;
+  drv = variant (haskellPackages.callPackage wikiScraper {}); 
 in
+pkgs.mkShell {
+  buildInputs = [ pkgs.cabal-install ];
+  inputsFrom = [ (if pkgs.lib.inNixShell then drv.env else drv) ];
+} 
 
-  if pkgs.lib.inNixShell then drv.env else drv
+
+
